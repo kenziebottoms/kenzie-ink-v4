87d73bd35ca30021d82611b592c4638cadfd3a72
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-02-26 17:07:08 -0600

    styled, ordered blog posts

--- assets/js/factory/rest.js
+++ assets/js/factory/rest.js
@@ -5,7 +5,7 @@ const angular = require("angular");
 angular.module("io").factory("RestFactory", function($q, $http, RESTDB) {
     let getBlog = () => {
         return $q((resolve, reject) => {
-            $http.get(`${RESTDB.url}/blog?apikey=${RESTDB.key}&h={"$orderby": "date"}`)
+            $http.get(`${RESTDB.url}/blog?apikey=${RESTDB.key}&sort=date&dir=-1`)
                 .then(({data}) => resolve(data))
                 .catch(err => reject(err));
         });
