0f6f424a423bedd0d204b77f3e3aef89a99279d3
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-02 11:13:12 -0600

    loading icon for blog pages

--- assets/js/ctrl/art.js
+++ assets/js/ctrl/art.js
@@ -5,6 +5,6 @@ const angular = require("angular");
 angular.module("io").controller("ArtCtrl", function($scope, RestFactory) {
     RestFactory.getArt()
         .then(posts => {
-            $scope.art = posts.slice(0,12);
+            $scope.blog = posts.slice(0,12);
         });
 });
--- assets/js/ctrl/code.js
+++ assets/js/ctrl/code.js
@@ -5,6 +5,6 @@ const angular = require("angular");
 angular.module("io").controller("CodeCtrl", function($scope, RestFactory) {
     RestFactory.getCode()
         .then(posts => {
-            $scope.art = posts.slice(0,12);
+            $scope.blog = posts.slice(0,12);
         });
 });
