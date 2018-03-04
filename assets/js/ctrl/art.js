4740be882a623ab665f3bbb4a00959cc28deb8b9
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-03 19:12:25 -0600

    added thumbnails, removed limit on blogs

--- assets/js/ctrl/art.js
+++ assets/js/ctrl/art.js
@@ -5,6 +5,7 @@ const angular = require("angular");
 angular.module("io").controller("ArtCtrl", function($scope, RestFactory) {
     RestFactory.getArt()
         .then(posts => {
-            $scope.blog = posts.slice(0,12);
+            $scope.blog = posts;
+            console.log($scope.blog);
         });
 });
