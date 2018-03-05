7d208f1ac8f5d475cd8c6887e1bfa7428aa4fdec
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-05 15:51:38 -0600

    enlarged logo a bit, starting on contact page

--- assets/js/ctrl/contact.js
+++ assets/js/ctrl/contact.js
@@ -2,6 +2,10 @@
 
 angular.module("io").controller("ContactCtrl", function($scope) {
     $scope.contact = {
-        "email": "kenziebottoms@gmail.com"
+        "email": "kenziebottoms@gmail.com",
+        "github": "https://github.com/kenziebottoms",
+        "instagram": "https://www.instagram.com/kenziebottoms",
+        "twitter": "https://twitter.com/kenziebottoms",
+        "linkedIn": "https://www.linkedin.com/in/mckenzie-bottoms"
     };
 });
