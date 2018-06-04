c3b69431ed31dbc1454d798ac414a362631925bd
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-06-04 12:34:18 -0500

    update linkedin link

--- dist/bundle.js
+++ dist/bundle.js
@@ -55,7 +55,7 @@ angular.module("io").controller("ContactCtrl", function($scope) {
         "github": "https://github.com/kenziebottoms",
         "instagram": "https://www.instagram.com/kenziebottoms",
         "twitter": "https://twitter.com/kenziebottoms",
-        "linkedin": "https://www.linkedin.com/in/mckenzie-bottoms",
+        "linkedin": "https://www.linkedin.com/in/kenzie-bottoms",
         "codepen": "https://codepen.io/kenziebottoms/",
         "artstation": "https://kenziebottoms.artstation.com/",
         "society6": "https://society6.com/claquesous",
