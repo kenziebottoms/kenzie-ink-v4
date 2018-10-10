c38b9ae41111f2ebaeafbbdadad20d3857301a71
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-10-10 10:43:41 -0500

    npm update

--- dist/bundle.js
+++ dist/bundle.js
@@ -352,7 +352,7 @@ angular.module("io").config(($stateProvider, $urlRouterProvider) => {
 });
 },{}],15:[function(require,module,exports){
 /**
- * @license AngularJS v1.7.2
+ * @license AngularJS v1.7.5
  * (c) 2010-2018 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -1064,7 +1064,7 @@ function sanitizeText(chars) {
 // define ngSanitize module and register $sanitize service
 angular.module('ngSanitize', [])
   .provider('$sanitize', $SanitizeProvider)
-  .info({ angularVersion: '1.7.2' });
+  .info({ angularVersion: '1.7.5' });
 
 /**
  * @ngdoc filter
