420f98eb1ef123b0783b71a024f872c202120e86
Kenzie Bottoms <kenziebottoms@gmail.com>
2019-01-30 11:50:30 -0600

    add prettierrc, change nrg to nrd, minify svg images

--- dist/bundle.js
+++ dist/bundle.js
@@ -65,11 +65,13 @@ angular.module("io").controller("ContactCtrl", function($scope) {
     };
 });
 },{}],6:[function(require,module,exports){
-"use strict";
+'use strict';
 
-angular.module("io").controller("HomeCtrl", function($scope) {
-    $scope.bio = '<p>Kenzie Bottoms is a feminist, artist, and developer born and raised in Nashville. They are always open to new opportunities, technical or creative (or better yet, both), and their default mindset, for better or worse, is usually, "Challenge accepted." They play roller derby under the name <a href="https://en.wikipedia.org/wiki/Ellen_Ripley" target="blank">Ellen R.I.P.ley</a> and skate with the <a href="https://www.nashvillerollergirls.com/" target="blank">Nashville Rollergirls</a>. They are a full-stack Node.js <a href="http://nashvillesoftwareschool.com/" target="blank">Nashville Software School</a> graduate, and have recently begun development work with <a href="https://fortyau.com/" target="blank">FortyAU</a>.</p>';
+angular.module('io').controller('HomeCtrl', function($scope) {
+  $scope.bio =
+    '<p>Kenzie Bottoms is a feminist, artist, and developer born and raised in Nashville. They are always open to new opportunities, technical or creative (or better yet, both), and their default mindset, for better or worse, is usually, "Challenge accepted." They skate with <a href="https://www.nashvillerollerderby.com/" target="blank">Nashville Roller Derby</a> under the name <a href="https://en.wikipedia.org/wiki/Ellen_Ripley" target="blank">Ellen R.I.P.ley</a>. They are a full-stack Node.js <a href="http://nashvillesoftwareschool.com/" target="blank">Nashville Software School</a> graduate, and have recently begun development work with <a href="https://fortyau.com/" target="blank">FortyAU</a>.</p>';
 });
+
 },{}],7:[function(require,module,exports){
 "use strict";
 
@@ -166,10 +168,11 @@ angular.module("io").factory("RestFactory", function($q, $http, RESTDB) {
 },{"lodash":17}],11:[function(require,module,exports){
 'use strict';
 
-angular.module("io").constant("RESTDB", {
-  "url": "https://kenziebottoms-c396.restdb.io/rest",
-  "key": "5a946ab3696ea8f017645933"
+angular.module('io').constant('RESTDB', {
+  url: 'https://kenziebottoms-c396.restdb.io/rest',
+  key: '5a946ab3696ea8f017645933',
 });
+
 },{}],12:[function(require,module,exports){
 "use strict";
 
