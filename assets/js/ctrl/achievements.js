a4326e10dacd022eb250f687ab5f62d5745ea4ba
Kenzie Bottoms <kenziebottoms@gmail.com>
2019-02-27 14:45:25 -0600

    update achievements

--- assets/js/ctrl/achievements.js
+++ assets/js/ctrl/achievements.js
@@ -13,7 +13,7 @@ angular.module('io').controller('AchievementCtrl', function($scope) {
       description: 'Find a bug by writing automated tests.',
     },
     {
-      title: "You're Welcome",
+      title: 'Voila',
       icon: 'cast',
       description: 'Demo a feature you built for a client.',
     },
@@ -36,35 +36,20 @@ angular.module('io').controller('AchievementCtrl', function($scope) {
         'Beat <a href="https://nanowrimo.org/about" target="blank">NaNoWriMo</a>.',
     },
     {
-      title: 'Beat The Game',
+      title: 'Color Coded',
+      icon: 'brush',
+      description: 'Write your own color theme for a text editor.',
+    },
+    {
+      title: 'Release Me',
       icon: 'access_time',
       description: 'Spend more than 50 hours on a single piece of art.',
-    },
-    {
-      title: 'Autocorrect',
-      icon: 'spellcheck',
-      description: "Correct a misspelled variable that isn't hurting anyone.",
-    },
-    {
-      title: 'Shrug Emoji',
-      icon: 'tag_faces',
-      description: 'Create 10 slack emojis.',
     },
     {
       title: 'Hacker Mode',
       icon: 'keyboard',
       description:
         'Write 20 aliases for <a href="https://ohmyz.sh/" target="blank">Zsh</a>.',
-    },
-    {
-      title: 'Pushover',
-      icon: 'assignment_turned_in',
-      description: 'Approve 20 pull requests.',
-    },
-    {
-      title: 'Debbie Downer',
-      icon: 'assignment_late',
-      description: 'Reject 1 pull request.',
     },
     {
       title: 'Mother May I?',
@@ -75,6 +60,16 @@ angular.module('io').controller('AchievementCtrl', function($scope) {
       title: 'Buckle Up',
       icon: 'send',
       description: 'Merge 20 branches into master.',
+    },
+    {
+      title: 'Pushover',
+      icon: 'assignment_turned_in',
+      description: 'Approve 20 pull requests.',
+    },
+    {
+      title: 'Shrug Emoji',
+      icon: 'tag_faces',
+      description: 'Create 10 slack emojis.',
     },
   ];
 });
