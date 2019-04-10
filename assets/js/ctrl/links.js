a1e6175c862943a628f861a03009fa72f3571bc6
Kenzie Bottoms <kenziebottoms@gmail.com>
2019-04-10 15:50:22 -0500

    refactor links page

--- assets/js/ctrl/links.js
+++ assets/js/ctrl/links.js
@@ -1,18 +1,52 @@
 'use strict';
 
 angular.module('io').controller('LinksCtrl', function($scope) {
-  $scope.links = {
-    email: 'kenziebottoms@gmail.com',
-    github: 'https://github.com/kenziebottoms',
-    instagram: 'https://www.instagram.com/kenziebottoms',
-    twitter: 'https://twitter.com/kenziebottoms',
-    linkedin: 'https://www.linkedin.com/in/kenzie-bottoms',
-    codepen: 'https://codepen.io/kenziebottoms/',
-    artstation: 'https://kenziebottoms.artstation.com/',
-    society6: 'https://society6.com/claquesous',
-    redbubble: 'https://www.redbubble.com/people/claquesous/shop',
-    artistsnclients: 'https://artistsnclients.com/people/kenziebottoms',
-    vsmarketplace:
-      'https://marketplace.visualstudio.com/publishers/kenziebottoms',
-  };
+  $scope.links = [
+    { name: 'Email', href: 'kenziebottoms@gmail.com', image: 'email.svg' },
+    {
+      name: 'GitHub',
+      href: 'https://github.com/kenziebottoms',
+      image: 'github.svg',
+    },
+    {
+      name: 'Instagram',
+      href: 'https://www.instagram.com/kenziebottoms',
+      image: 'insta.svg',
+    },
+    {
+      name: 'Twitter',
+      href: 'https://twitter.com/kenziebottoms',
+      image: 'twitter.svg',
+    },
+    {
+      name: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/kenzie-bottoms',
+      image: 'linkedin.svg',
+    },
+    {
+      name: 'CodePen',
+      href: 'https://codepen.io/kenziebottoms/',
+      image: 'codepen.svg',
+    },
+    {
+      name: 'Visual Studio Marketplace',
+      href: 'https://marketplace.visualstudio.com/publishers/kenziebottoms',
+      image: 'vscode.png',
+    },
+    {
+      name: 'ArtStation',
+      href: 'https://kenziebottoms.artstation.com/',
+      image: 'artstation.svg',
+    },
+    {
+      name: 'Society6',
+      href: 'https://society6.com/claquesous',
+      image: 'society6.png',
+    },
+    {
+      name: 'Artists&Clients',
+      href: 'https://artistsnclients.com/people/kenziebottoms',
+      image: 'artistsnclients.png',
+    },
+  ];
 });
